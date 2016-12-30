f50067ec6988e694427af3d11949475ade3ae734
Ikey Doherty <ikey@solus-project.com>
2016-12-30 00:29:33 +0000

    Add skeleton for TileSheet
    The TileSheet will be used to load an sdl surface from an image file
    and render offset rectangles from it onto the target SDL_Renderer.
    Currently this code is a simple no-op.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/snake/tilesheet.h
@@ -0,0 +1,49 @@
+/*
+ * This file is part of vacation-cruft
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <SDL.h>
+
+typedef struct TileSheet {
+        int tile_size;
+        SDL_Surface *surface;
+} TileSheet;
+
+/**
+ * Create a new tilesheet for the given path and tile size
+ */
+TileSheet *tile_sheet_new(const char *path, int tile_size, SDL_Renderer *render,
+                          SDL_Window *window);
+
+/**
+ * Render a given tile by column/row offset
+ */
+void tile_sheet_render(TileSheet *sheet, int column, int row, SDL_Renderer *render,
+                       SDL_Rect target);
+
+/**
+ * Free a previously created TileSheet
+ */
+void tile_sheet_free(TileSheet *self);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
